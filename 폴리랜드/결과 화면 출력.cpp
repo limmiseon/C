3d53dd027895494ad70b9f5105de16b0e7ef7f6b
limmieseon <helloaltjs@gmail.com>
2022-03-24 08:50:21 +0900

    우대권 판매현황 결과 화면 출력

--- 폴리랜드/결과 화면 출력.cpp
+++ 폴리랜드/결과 화면 출력.cpp
@@ -13,6 +13,10 @@ int main()
 	int baby_comp = 0, child_comp = 0, teen_comp = 0, adult_comp = 0, old_comp = 0, newborn_comp = 0,
 		baby_park = 0, child_park = 0, teen_park = 0, adult_park = 0, old_park = 0, newborn_park = 0;	
 	int sales_comp = 0, sales_park = 0;
+	
+	int totalSales = 0,
+		discount_no = 0, discount_disable = 0, discount_merit = 0, discount_army = 0, discount_pregnant = 0, discount_multichild = 0;
+	
 		
 	while(fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d", &year[count], &month[count], &day[count], &ArrticketSelect_voucher[count],
 	&ArrticketSelect_voucher_kind[count], &ArrageCase[count], &Arrcount[count], &ArrpriceResult[count], &ArrdiscountRate[count]) != -1)
@@ -37,7 +41,7 @@ int main()
 	{
 		if(ArrticketSelect_voucher[index] == 1)
 		{
-			amount_ticketselect_comp ++;
+			amount_ticketselect_comp += Arrcount[index];
 			sales_comp += ArrpriceResult[index];
 			
 			if(ArrageCase[index] == 1)
@@ -68,7 +72,7 @@ int main()
 		
 		if(ArrticketSelect_voucher[index] == 2)
 		{
-			amount_ticketselect_park ++;
+			amount_ticketselect_park += Arrcount[index];
 			sales_park += ArrpriceResult[index];
 			
 			if(ArrageCase[index] == 1)
@@ -105,8 +109,48 @@ int main()
 	printf("파크이용권 총 %d매\n", amount_ticketselect_park);
 	printf("유아 %d매, 어린이 %d매, 청소년 %d매, 어른 %d매, 노인 %d매\n", baby_park, child_park, teen_park, adult_park, old_park);
 	printf("종합이용권 매출 : %d원\n\n", sales_park);	
-	
 	printf("--------------------------------------------------------------------\n\n\n");
+	
+	printf("===== 우대권 판매 현황 =====\n\n");
+	
+	for(int index = 0; index < count; index++)
+	{
+		totalSales += Arrcount[index];
+		
+		if(ArrdiscountRate[index] == 1)
+		{
+			discount_no += Arrcount[index];
+		}
+		else if(ArrdiscountRate[index] == 2)
+		{
+			discount_disable += Arrcount[index];
+		}
+		else if(ArrdiscountRate[index] == 3)
+		{
+			discount_merit += Arrcount[index];
+		}
+		else if(ArrdiscountRate[index] == 4)
+		{
+			discount_army += Arrcount[index];
+		}
+		else if(ArrdiscountRate[index] == 5)
+		{
+			discount_pregnant += Arrcount[index];
+		}
+		else
+		{
+			discount_multichild += Arrcount[index];
+		}
+	}
+	
+	printf("총 판매 티켓수     :   %d매\n", totalSales);
+	printf("우대 없음          :   %d매\n", discount_no);
+	printf("장애인             :   %d매\n", discount_disable);
+	printf("국가유공자         :   %d매\n", discount_merit);
+	printf("휴가장병           :   %d매\n", discount_army);
+	printf("임산부             :   %d매\n", discount_pregnant);
+	printf("다둥이 행복카드    :   %d매\n\n", discount_multichild);
+	printf("----------------------------\n\n");			
 	
 	return 0;
 }
